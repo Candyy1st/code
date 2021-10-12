6ca381d0bcfcdea3c3cc0849e64cd4d272ecbf21
Candyy1st <candradewa1981@gmail.com>
2021-10-12 08:28:37 +0700

    Finish real data from BMKG.go.id

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 """
-Testing Perulangan
+Aplikasi deteksi Gempa
+MOdularisasi dengan Function
 """
+from deteksi_gempa import ekstraksi_data, tampilkan_data
 
-total = 10
-books_read = 0
-print('Mom said : "You must reading 10 books"')
-
-# Using For
-for books_read in range(1,11):
-    print(f'Buku ke-{books_read} sudah dibaca')
-
-#U Using While
-while books_read < total:
-    books_read = books_read + 1
-    print(f'Buku ke-{books_read} sudah dibaca')
-
-print(f'Buku yang sudah dibaca = {books_read}')
+if __name__ == '__main__':
+    print('Aplikasi Utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
